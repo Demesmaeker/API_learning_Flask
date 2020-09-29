27f3030b7bbf05ea7c18d4757b2dac6b2096a56f
Morgane Demesmaeker <morgane.demesmaeker@gmail.com>
2020-09-29 16:48:57 +0200

    rewrite as class

--- app.py
+++ app.py
@@ -1,19 +1,21 @@
 from flask import Flask, request, jsonify
+from flask_restx import Api, Resource, fields
 from flask_sqlalchemy import SQLAlchemy
-from flask_marshmallow import Marshmallow
 import os
 from random import randint
 
 # Init app
 app = Flask(__name__)
+api = Api(app, version='1.0', title='Learning API',
+    description='API done when learning the Art of API Crafting',
+)
 basedir = os.path.abspath(os.path.dirname(__file__))
+
 
 # Database
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'db.sqlite')
 db = SQLAlchemy(app)
 
-# Init Marshmallow
-ma = Marshmallow(app)
 
 # Seller Class/model
 class Seller(db.Model):
@@ -26,54 +28,53 @@ class Seller(db.Model):
         self.VAT = VAT
 
 
-# Seller Schema
-class SellerSchema(ma.Schema):
-    class Meta:
-        fields = ('id', 'name', 'VAT')
 
-seller_schema = SellerSchema(strict=True)
-sellers_schema = SellerSchema(many=True, strict=True)
+@api.route('/')
+class home(Resource):
+    def get(self):
+        return jsonify({'message' : 'Hello World'})
 
 
+@api.route('/status', methods=['GET'])
+class status(Resource):
+    def get(self):
+        result = {
+            "status" : True,
+            "message" : "The server is alive! Please, don't kill it..."
+        }
+        return jsonify(result)
 
-@app.route('/')
-def home():
-   return jsonify({'message' : 'Hello World'})
-
-@app.route('/status', methods=['GET'])
-def status():
-    result = {
-        "status" : True,
-        "message" : "The server is alive! Please, don't kill it..."
-    }
-    return jsonify(result)
-
-@app.route('/login', methods=['POST'])
-def login():
-    log_info = request.get_json()
-    username = log_info["username"]
-    password_length = len(log_info["password"])
-    return f"Login success for user {username} with password from length: {password_length}!"
+@api.route('/login', methods=['POST'])
+class login(Resource):
+    def post(self):
+        log_info = request.get_json()
+        username = log_info["username"]
+        password_length = len(log_info["password"])
+        return f"Login success for user {username} with password from length: {password_length}!"
 
 
-@app.route('/predict/<int:seller_avaible>/<string:month>/<int:customer_visiting_website>', methods=['GET'])
-def predict(month, customer_visiting_website, seller_avaible):
-    number = randint(2000, 5000)
-    return f"{number}"
+@api.route('/predict/<int:seller_avaible>/<string:month>/<int:customer_visiting_website>', methods=['GET'])
+class predict(Resource):
+    def get(self, month, customer_visiting_website, seller_avaible):
+        number = randint(2000, 5000)
+        return f"{number}"
 
 
-@app.route('/sellers', methods=['GET', 'POST', 'PATCH', 'DELETE'])
-def seller():
-    if request.method == 'GET' :
+@api.route('/sellers', methods=['GET', 'POST', 'PATCH', 'DELETE'])
+class seller(Resource):
+    def get(self):
         sellers = "test get"
         return sellers
-    elif request.method == 'POST' :
+
+    def post(self):
         sellers = "test post"
         return sellers
-    elif request.method == 'PATCH' :
+    
+    def patch(self):
         sellers = "test patch"
         return sellers
-    else:
+    
+    def delete(self):
         sellers = "test delete"
         return sellers
 
